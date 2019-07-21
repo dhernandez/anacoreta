99cf0c7e400d1f2d502a9a12f5c4f4940cf74b6e
Daniel Hernández <danielhernandezmarin@gmail.com>
2019-07-21 15:00:22 +0200

    Some debug helps for testing port in Heroku

--- app/client/SocketIOClient.py
+++ app/client/SocketIOClient.py
@@ -6,10 +6,24 @@ class SocketIOClient:
     @staticmethod
     def emit_file_added():
         def run_client():
+            print('Emitting files added')
+            print('PORT: {}'.format(os.getenv('PORT')))
             from socketIO_client import SocketIO
             socketIO = SocketIO('localhost', os.getenv('PORT'))
             socketIO.emit('files added', broadcast=True)
+            print('Emitted!')
+            socketIO.disconnect()
+
+        def run_client_port_80():
+            print('Emitting files added (port 80)')
+            from socketIO_client import SocketIO
+            socketIO = SocketIO('localhost', os.getenv('PORT'))
+            socketIO.emit('files added', broadcast=True)
+            print('Emitted! (port 80)')
             socketIO.disconnect()
 
         thread = threading.Thread(target=run_client)
         thread.start()
+
+        thread_test_80 = threading.Thread(target=run_client_port_80)
+        thread_test_80.start()
